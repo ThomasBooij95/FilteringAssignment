25c9da54345d947d8972948500bacc9d79058532
NemeZisHun <bakos.mate94@gmail.com>
2018-01-12 16:33:42 +0100

    Started the .m file, worked out 3.1-2 on paper

--- FilteringAssignment.m
+++ FilteringAssignment.m
@@ -5,4 +5,28 @@ clc
 load turbulenceData.mat
 load systemMatrices.mat
 
-[ sigma ] = AOloop_nocontrol(phiIdent{1},SNR,H,G)
+%%
+%3.1
+
+%check rank of G
+rk=rank(G)
+maxrk=min(size(G))
+if rk == maxrk
+    disp('Matrix G is full rank!')
+else
+    disp('Matrix G is NOT full rank!')
+end
+
+%Single value decomp
+[U,S,V] = svd(G);
+SumSV=S(1:rk,1:rk);
+
+
+%Plotting of the SVs
+fig1=figure('units','normalized','outerposition',[0 0 1 1])
+semilogy(SumSV,'+r')
+
+
+
+
+%[ sigma ] = AOloop_nocontrol(phiIdent{1},SNR,H,G)
